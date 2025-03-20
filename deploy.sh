6d2b8a6794ca18ce02320a6d2e4747ade82b841f
Mathav Ramalingam <mathavramalingam1608@gmail.com>
2025-03-20 15:24:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u mathavramalingam -p dckr_pat_6vZaSzV9PzQ8NPi7ZkXIhGwRRsg
-    docker tag test mathavramalingam/devops
+    docker tag test1 mathavramalingam/devops
     docker push mathavramalingam/devops
     docker-compose up -d
